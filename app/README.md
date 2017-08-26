fdabe85d90704af045ab093e0346c8f05f61c46a
meldon <tfo2011@gmail.com>
2017-08-27 01:21:17 +0300

    asd

--- app/README.md
+++ app/README.md
@@ -1,1 +1,7 @@
 http://localhost:8000/api/all_feature/1 - get all feature
+
+
+
+
+
+sudo systemctl stop apache2.service
--- app/web/angularModul/controller.js
+++ app/web/angularModul/controller.js
@@ -1,47 +1,83 @@
 'use strict';
 
-var taApp = angular.module('taApp', ['ngRoute', 'ngResource']);
+/* Controllers */
+var phonecatApp = angular.module('phonecatApp', ['ngRoute', 'ngResource']);
 
-taApp.config(['$routeProvider', '$locationProvider', function ($routeProvide, $locationProvider) {
+phonecatApp.config(['$routeProvider', '$locationProvider', function ($routeProvide, $locationProvider) {
     $routeProvide
         .when('/', {
             templateUrl: '/angularModul/template/home.html',
-            controller: 'HomeCtrl'
+            controller: 'PhoneListCtrl'
         })
-        // .when('/about', {
-        //     templateUrl: 'template/about.html',
-        //     controller: 'AboutCtrl'
-        // })
-        // .when('/contact', {
-        //     templateUrl: 'template/contact.html',
-        //     controller: 'ContactCtrl'
-        // })
+        .when('/about', {
+            templateUrl: '/angularModul/template/about.html',
+            controller: 'AboutCtrl'
+        })
         .when('/features', {
-        // .when('/features/:featureId', {
-            templateUrl: 'angularModul/template/features.html',
+            templateUrl: '/angularModul/template/about.html',
             controller: 'FeaturesCtrl'
         })
-    // .otherwise({
-    //     redirectTo: '/'
-    // });
-}]);
-
-taApp.controller('FeaturesCtrl', ['$scope', '$http', '$location','Features', function ($scope, $http, $location,Features) {
-    // console.log('FeaturesCtrl');
-    $scope.phones=Features.query();
-
-}]);
-
-taApp.controller('HomeCtrl', ['$scope', '$http', '$location','Features', function ($scope, $http, $location,Features) {
-    // console.log('HomeCtrl');
-    $scope.phones=Features.query();
-
-}]);
-
-taApp.factory('Features', [
-    '$resource', function ($resource) {
-        return $resource('/api/all_feature/:featureId',{
-            featureId: null
+        .when('/contact', {
+            templateUrl: '/angularModul/template/contact.html',
+            controller: 'ContactCtrl'
         })
+        .when('/phones/:phoneId', {
+            templateUrl: '/angularModul/template/phone-detail.html',
+            controller: 'PhoneDetailCtrl'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+}]);
+
+/* Filter */
+phonecatApp.filter('checkmark', function () {
+    return function (input) {
+        return input ? 'u2713' : 'u2718';
     }
-]);
+});
+
+phonecatApp.controller('PhoneListCtrl', ['$scope', '$http', '$location', function ($scope, $http, $location) {
+    console.log('PhoneListCtrl');
+
+}]);
+//About Controller
+phonecatApp.controller('FeaturesCtrl', ['$scope', '$http', '$location', function ($scope, $http, $location) {
+    console.log('FeaturesCtrl');
+}]);
+//About Controller
+phonecatApp.controller('AboutCtrl', ['$scope', '$http', '$location', function ($scope, $http, $location) {
+ console.log('AboutCtrl');
+}]);
+//Contact Controller
+phonecatApp.controller('ContactCtrl', ['$scope', '$http', '$location', function ($scope, $http, $location) {
+
+}]);
+
+
+//Phone Detail Controller
+phonecatApp.controller('PhoneDetailCtrl', ['$scope', '$http', '$location', '$routeParams', function ($scope, $http, $location, $routeParams) {
+    $scope.phoneId = $routeParams.phoneId;
+
+    // Phone.get({phoneId: $routeParams.phoneId},function (data) {
+    //     $scope.phone = data;
+    //     $scope.mainImageUrl = data.images[0];
+    // });
+// var url = 'phones/' + $routeParams.phoneId + '.json';
+// $http.get(url).success(function (data) {
+// $scope.phone = data;
+// $scope.mainImageUrl = data.images[0];
+// });
+
+}]);
+
+
+// phonecatApp.factory('Phone', [
+//     '$resource', function ($resource) {
+//         return $resource('phones/:phoneId.:format',{
+//             phoneId: 'phones',
+//             format: 'json',
+//             someFunc: 'ggggg'
+//         })
+//     }
+// ]);
