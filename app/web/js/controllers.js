2cb5b33b1f5c77727018557b7d97dc29582902c0
meldon <tfo2011@gmail.com>
2017-08-27 02:46:12 +0300

    fix

--- app/web/js/controllers.js
+++ app/web/js/controllers.js
@@ -1,93 +1,43 @@
 'use strict';
 
 /* Controllers */
-var phonecatApp = angular.module('phonecatApp', ['ngRoute', 'ngResource']);
+var taApp = angular.module('taApp', ['ngRoute', 'ngResource']);
 
-phonecatApp.config(['$routeProvider', '$locationProvider', function ($routeProvide, $locationProvider) {
+taApp.config(['$routeProvider', '$locationProvider', function ($routeProvide, $locationProvider) {
     $routeProvide
         .when('/', {
             templateUrl: 'template/home.html',
-            controller: 'PhoneListCtrl'
-        })
-        .when('/about', {
-            templateUrl: 'template/about.html',
-            controller: 'AboutCtrl'
+            controller: 'HomeCtrl'
         })
         .when('/features', {
-            templateUrl: 'template/about.html',
-            controller: 'FeaturesCtrl'
-        })
-        .when('/contact', {
-            templateUrl: 'template/contact.html',
-            controller: 'ContactCtrl'
-        })
-        .when('/phones/:phoneId', {
-            templateUrl: 'template/phone-detail.html',
-            controller: 'PhoneDetailCtrl'
+            templateUrl: 'template/feature.html',
+            controller: 'FeatureCtrl'
         })
         .otherwise({
             redirectTo: '/'
         });
 }]);
 
-/* Filter */
-phonecatApp.filter('checkmark', function () {
-    return function (input) {
-        return input ? '\u2713' : '\u2718';
-    }
-});
 
-phonecatApp.controller('PhoneListCtrl', ['$scope', '$http', '$location','Phone', function ($scope, $http, $location, Phone) {
-    //console.log('$location.url() - ', $location.url());
-    //console.log('$location.path() - ', $location.path());
-    //console.log('$location.search() - ', $location.search());
-    //console.log('$location.hash() - ', $location.hash());
-
-    // $http.get('phones/phones.json').success(function (data, status, headers, config) {
-    //     $scope.phones = data;
-    // });
-
-    $scope.phones=Phone.query();
-
+taApp.controller('HomeCtrl', ['$scope', '$http', '$location', function ($scope, $http, $location) {
 
 }]);
-//About Controller
-phonecatApp.controller('FeaturesCtrl', ['$scope', '$http', '$location', function ($scope, $http, $location) {
-console.log('asdasd');
-}]);
-//About Controller
-phonecatApp.controller('AboutCtrl', ['$scope', '$http', '$location', function ($scope, $http, $location) {
 
-}]);
-//Contact Controller
-phonecatApp.controller('ContactCtrl', ['$scope', '$http', '$location', function ($scope, $http, $location) {
-
-}]);
 
 
 //Phone Detail Controller
-phonecatApp.controller('PhoneDetailCtrl', ['$scope', '$http', '$location', '$routeParams','Phone', function ($scope, $http, $location, $routeParams,Phone) {
-    $scope.phoneId = $routeParams.phoneId;
-
-    Phone.get({phoneId: $routeParams.phoneId},function (data) {
-        $scope.phone = data;
-        $scope.mainImageUrl = data.images[0];
-    });
-    // var url = 'phones/' + $routeParams.phoneId + '.json';
-    // $http.get(url).success(function (data) {
-    //     $scope.phone = data;
-    //     $scope.mainImageUrl = data.images[0];
-    // });
-
+taApp.controller('FeatureCtrl', ['$scope', '$http', '$location', '$routeParams','Feature', function ($scope, $http, $location, $routeParams,Feature) {
+    $scope.featureId = $routeParams.featureId;
+    $scope.features = Feature.query();
 }]);
 
 
-phonecatApp.factory('Phone', [
+
+
+taApp.factory('Feature', [
     '$resource', function ($resource) {
-        return $resource('phones/:phoneId.:format',{
-            phoneId: 'phones',
-            format: 'json',
-            someFunc: 'ggggg'
+        return $resource('api/all_feature/:featureId',{
+            featureId: '1'
         })
     }
 ]);
